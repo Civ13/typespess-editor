3d66bb46ee67da1bb3d34e7a74b56e8e0a01f48f
Taislin <cariosecus@gmail.com>
2021-02-07 01:00:25 +0000

    some fixes to code

--- spec/typespess-editor-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const TypespessEditorView = require('../lib/typespess-editor-view');
-
-describe('TypespessEditorView', () => {
-	it('has one valid test', () => {
-		expect('life').toBe('easy');
-	});
-});
